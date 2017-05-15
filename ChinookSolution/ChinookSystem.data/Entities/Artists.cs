959a01c45ffce2cd3e26dc083e4de9ba5516b63b
Allan Anderson <allana@shaw.ca>
2017-05-14 23:42:19 -0700

    aaa

---
+++ ChinookSolution/ChinookSystem.data/Entities/Artists.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+    #endregion
+namespace ChinookSystem.data.Entities
+{
+          [Table("Artists")]
+    public class Artists
+    {
+        [Key]
+        public int ArtistId { get; set; }
+        public string Name { get; set;}
+
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+
+}
---
+++ ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+#endregion
+
+namespace ChinookSystem.DAL
+{
+    //This is an internal class for security reasons
+    //Access is restricted to within this class library project
+    //Inherits DbContext for Entity Framework, which requires
+    // System.Data.Entity
+
+    internal class ChinookContext : DbContext
+    {
+        //Pass the coonection string name to the
+        //  DbContext using : base()
+        public ChinookContext() : base("ChinookDB")
+        { }
+    }
+}
